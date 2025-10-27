0b397a9f72c8b3c30b1b4798a33f39c256f386df
at-github-th <at.github.th@gmail.com>
2025-10-27 17:02:10 +0000

    fix: enable CORS + correct ports + working example routes

--- api/main.go
+++ api/main.go
@@ -1,11 +1,14 @@
-
 package main
-import ("github.com/gofiber/fiber/v2")
+import (
+  "github.com/gofiber/fiber/v2"
+  "github.com/gofiber/fiber/v2/middleware/cors"
+)
 func main() {
-    app := fiber.New()
-    app.Get("/", func(c *fiber.Ctx) error { return c.JSON(fiber.Map{"name":"Real-time Device/Kiosk Monitor API","ok":true}) })
-    app.Get("/api/devices", func(c *fiber.Ctx) error {
-        return c.JSON([]fiber.Map{{"id":"D-1","type":"RFID","alive":true},{"id":"D-2","type":"Locker","alive":false}})
-    })
-    app.Listen(":5103")
+  app := fiber.New()
+  app.Use(cors.New())
+  app.Get("/", func(c *fiber.Ctx) error { return c.JSON(fiber.Map{"name":"Device/Kiosk Monitor API","ok":true}) })
+  app.Get("/api/devices", func(c *fiber.Ctx) error {
+    return c.JSON([]fiber.Map{{{"id":"D-1","type":"RFID","alive":true}},{{"id":"D-2","type":"Locker","alive":false}}})
+  })
+  app.Listen(":5103")
 }
